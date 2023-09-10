83ba09d881df952146403116de856b986608a80a
tubstrr <me@jonknoll.dev>
2023-09-10 15:00:12 -0500

    last test of the day

--- api/test.js
+++ api/test.js
@@ -1,7 +1,23 @@
 export const config = {
  runtime: "experimental-edge",
 };
+import { parseCookies, makeHash, fetchHTML } from "../helpers";
+export default async function MyEdgeFunction(request, context) {
+ // This is test
+ const frozenHeaders = JSON.parse(JSON.stringify(request.headers));
+ const cookies = parseCookies(frozenHeaders);
 
-export default function MyEdgeFunction(request, context) {
- return new Response(`Hello, from ${request.url} I'm an Edge Function!`);
+ const url = new URL(request.url);
+
+ const fetchPage = await fetchHTML(url.pathname + url.hash + url.search, cookies);
+ if (fetchPage instanceof Response) {
+  return fetchPage;
+ } else {
+  return new Response(fetchPage, {
+   status: 200,
+   headers: {
+    "Content-Type": "text/html",
+   },
+  });
+ }
 }
